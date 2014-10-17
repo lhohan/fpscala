2748570b97cd967ee170da241d237fab8082b5be
lhohan <github@lhoest.eu>
2014-10-17 19:19:54 +0200

    add scalatest to build

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,8 @@ import Keys._
 object FPInScalaBuild extends Build {
   val opts = Project.defaultSettings ++ Seq(
     scalaVersion := "2.10.3",
-    resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+    resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.2.1" % "test"
   )
 
   lazy val root =
