3e1ef46b511d2608b6ddd75487bddfbfc970d113
lhohan <github@lhoest.eu>
2014-10-17 19:36:10 +0200

    Added tests for isSorted[A] for different type

--- exercises/src/test/scala/fpinscala/gettingstarted/Tests.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/Tests.scala
@@ -8,7 +8,7 @@ import org.scalatest.FunSuite
 class Tests extends FunSuite{
 
 
-  test("isSorted"){
+  test("isSorted - Int"){
     import fpinscala.gettingstarted.PolymorphicFunctions.isSorted
     def gt1 = (x:Int, y:Int) => x <= y
     assertResult(true, "empty") {isSorted(Array(),gt1)}
@@ -19,6 +19,17 @@ class Tests extends FunSuite{
     assertResult(true, "sorted - 2") {isSorted(Array(-1,3,5,6,6),gt1)}
     assertResult(false, "unsorted - 1") {isSorted(Array(-1,7,5),gt1)}
     assertResult(false, "unsorted - 2") {isSorted(Array(4,3),gt1)}
+  }
+
+  test("isSorted - String"){
+    import fpinscala.gettingstarted.PolymorphicFunctions.isSorted
+    def gt1(x:String, y:String) = x.length <= y.length
+    assertResult(true, "empty") {isSorted(Array(),gt1)}
+    assertResult(true, "one") {isSorted(Array("abc"),gt1)}
+    assertResult(true, "sorted 2") {isSorted(Array("abc","abcdef"),gt1)}
+    assertResult(true, "sorted 2 - same") {isSorted(Array("ab","ab"),gt1)}
+    assertResult(true, "sorted - 1") {isSorted(Array("a","ab","xy","qwerty","qwertyuiop"),gt1)}
+    assertResult(false, "unsorted - 1") {isSorted(Array("abcd","abcdef","a"),gt1)}
   }
 
 }
