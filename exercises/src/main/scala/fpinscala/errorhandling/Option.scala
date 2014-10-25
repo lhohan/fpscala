83c15e97cfeb2ea8576ad9e06d4446e423312e52
lhohan <github@lhoest.eu>
2014-10-25 19:31:39 +0200

    ex. 4.1 nicer orElse - formatting

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,7 +1,9 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.{Either => _, Option => _, Some => _}
+
+// hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
@@ -9,21 +11,25 @@ sealed trait Option[+A] {
     case Some(a) => Some(f(a))
   }
 
-  def getOrElse[B>:A](default: => B): B = this match {
+  def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
 
   def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
-    case None => ob
-    case _ => this
-  }
+  //  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
+  //    case None => ob
+  //    case _ => this
+  //  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this map (Some(_)) getOrElse (ob)
 
   def filter(f: A => Boolean): Option[A] = sys.error("todo")
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
 object Option {
@@ -33,7 +39,9 @@ object Option {
       val x = 42 + 5
       x + y
     }
-    catch { case e: Exception => 43 } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    catch {
+      case e: Exception => 43
+    } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
   }
 
   def failingFn2(i: Int): Int = {
@@ -41,15 +49,18 @@ object Option {
       val x = 42 + 5
       x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
     }
-    catch { case e: Exception => 43 }
+    catch {
+      case e: Exception => 43
+    }
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+
   def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
--- exercises/src/test/scala/fpinscala/ch04_ErrorHandlingTests.scala
+++ exercises/src/test/scala/fpinscala/ch04_ErrorHandlingTests.scala
@@ -7,30 +7,30 @@ import org.scalatest.FunSuite
  */
 class ch04_ErrorHandlingTests extends FunSuite {
 
-  test("map"){
+  test("map") {
     import fpinscala.errorhandling._
-    def f(x:Int) = x+1
+    def f(x: Int) = x + 1
 
     assertResult(None, "none")(None.map(f))
     assertResult(Some(6), "some")(Some(5).map(f))
   }
 
-  test("getOrElse"){
+  test("getOrElse") {
     import fpinscala.errorhandling._
 
     assertResult("x", "none")(None.getOrElse("x"))
     assertResult(5, "some")(Some(5).getOrElse(42))
   }
 
-  test("flatMap"){
+  test("flatMap") {
     import fpinscala.errorhandling._
-    def f(x:Int) = Some(x+1)
+    def f(x: Int) = Some(x + 1)
 
     assertResult(None, "none")(None.flatMap(f))
     assertResult(Some(6), "some")(Some(5).flatMap(f))
   }
 
-  test("orElse"){
+  test("orElse") {
     import fpinscala.errorhandling._
 
     assertResult(Some("default option value"), "none")(None.orElse(Some("default option value")))
