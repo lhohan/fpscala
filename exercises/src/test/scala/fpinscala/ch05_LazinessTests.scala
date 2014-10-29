74e11d047424050d335a405f542c0deb510b67c1
lhohan <github@lhoest.eu>
2014-10-29 20:32:01 +0100

    ex. 5.1 - toList - added test ex. 5.2 - take test case

--- exercises/src/test/scala/fpinscala/ch05_LazinessTests.scala
+++ exercises/src/test/scala/fpinscala/ch05_LazinessTests.scala
@@ -11,6 +11,14 @@ import org.scalatest.FunSuite
       import fpinscala.laziness._
 
       assertResult(List(1,2,3), "toList")(Stream(1,2,3).toList)
+      assertResult(List(), "empty")(Stream().toList)
+    }
+
+    test("take") {
+      import fpinscala.laziness._
+
+      assertResult(List(1,2), "base")(Stream(1,2,3).take(2).toList)
+      assertResult(List(), "empty")(Stream().take(0).toList)
     }
 
 }
