bed4ea3e4c2e3bbe9709218780d2e029ce8f583c
lhohan <github@lhoest.eu>
2014-11-06 19:32:44 +0100

    ex. 5.13 - zipWith shorter

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -116,9 +116,8 @@ trait Stream[+A] {
   }
 
   def zipWith[B, C](s2: Stream[B])(f: (A, B) => C): Stream[C] = unfold((this, s2)) {
-    case (Empty, _) => None
-    case (_, Empty) => None
     case (Cons(h1, t1), Cons(h2, t2)) => Some((f(h1(), h2()), (t1(), t2())))
+    case _ => None
   }
 
   def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = unfold((this, s2)) {
