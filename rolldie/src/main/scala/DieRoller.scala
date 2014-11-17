a93ce32aed99c50f39bf6bff8baca44544c02276
lhohan <github@lhoest.eu>
2014-11-17 19:26:39 +0100

    Making rollDie functional - refactoring towards reproducible test. Test **constitently** fails.
    rollDie now takes a random number generator as a parameter and returns a pair of the next value and the next state of the RNG

--- rolldie/src/main/scala/DieRoller.scala
+++ rolldie/src/main/scala/DieRoller.scala
@@ -1,11 +1,32 @@
-import scala.util.Random
+
 
 /**
  * Created by hans on 16/11/14.
  */
 object DieRoller {
-  def rollDie: Int = {
-    val rng = Random
-    rng.nextInt(6)
+
+  // The key to recovering referential transparency: make state updates explicit.
+  // Return the new state along with the value that we are generating
+  trait RNG {
+    def nextInt: (Int, RNG)
   }
+
+  // Implementation of RNG that will do the trick. That's all we need for now. Detail in the book.
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
+      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
+      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+    }
+  }
+
+  // keeping it simple, book's impl. is different but more complex
+  def nonNegativeIntLessThan(n: Int)(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    ((if (i < 0) -i else i) % n, r)
+  }
+
+  def rollDie: RNG => (Int, RNG) = nonNegativeIntLessThan(6)
+
 }
--- rolldie/src/test/scala/DieRollerTest.scala
+++ rolldie/src/test/scala/DieRollerTest.scala
@@ -4,10 +4,17 @@ import org.scalatest.FunSuite
  * Created by hans on 16/11/14.
  */
 class DieRollerTest extends FunSuite {
+
   import DieRoller._
 
   test("roll should be between in range 1 to 6") {
-    val _3Rolls = (1 to 3).map(x => rollDie)
+    val _3Rolls = Stream.iterate(rollDie(Simple(5)), 3) {
+      x =>
+        val (i, r) = x
+        val (i2, r2) = rollDie(r)
+        (i, r2)
+    }.map(_._1).toList
+
     _3Rolls.foreach { roll =>
       assert(roll > 0 && roll <= 6, s"error: $roll")
     }
