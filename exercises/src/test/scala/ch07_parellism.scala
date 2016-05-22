cbb35cf6b50dc916a54ca77dc40bb7f96e83cc3b
Hans L'hoest <hans.lhoest@mendix.com>
2016-05-22 21:21:26 +0200

    Add exercises.

---
+++ exercises/src/test/scala/ch07_parellism.scala
@@ -0,0 +1,47 @@
+import org.scalatest._
+import org.scalatest.concurrent.Timeouts
+import org.scalatest.time.Span
+
+class Par2Tests extends FunSuite with Timeouts {
+  import fpinscala.parallelism.Par2._
+  import scala.concurrent.duration._
+  import scala.concurrent._
+
+  test("parMap") {
+    implicit val ec = scala.concurrent.ExecutionContext.global
+    val p = parMap(List(3, 2, 1)) { i =>
+      Thread.sleep(i * 1000);
+      //println(i);
+    }
+
+    // we wait less than 3 + 2 + 1 seconds to check if parMap indeed runs in parallel
+    Await.result(p.run, 3.5 seconds)
+  }
+
+  test("parSum") {
+    implicit val ec = scala.concurrent.ExecutionContext.global
+    import fpinscala.parallelism.Examples2._
+
+    val p = parSum(Vector(100, 200, 300))
+    assert(600 === Await.result(p.run, 100 millis))
+
+  }
+
+  test("parSumExec") {
+    implicit val ec = scala.concurrent.ExecutionContext.global
+    import fpinscala.parallelism.Examples2._
+
+    val p = parSumExec(Vector(100, 200, 300))
+    assert(600 === Await.result(p.run, 100 millis))
+  }
+
+  test("parMaxExec") {
+    implicit val ec = scala.concurrent.ExecutionContext.global
+    import fpinscala.parallelism.Examples2._
+
+    val p = parMaxExec(Vector(100, 200, 300))
+    assert(Some(300) === Await.result(p.run, 100 millis))
+    assert(None === Await.result(parMaxExec(Vector()).run, 100 millis))
+  }
+
+}
