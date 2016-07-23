a71714f8407e75e64c9d23bba4a181fd10646f84
Hans L'hoëst <github@lhoest.eu>
2016-07-23 11:33:39 +0200

    Formatting.

--- exercises/src/test/scala/fpinscala/ch10_MonoidsTests.scala
+++ exercises/src/test/scala/fpinscala/ch10_MonoidsTests.scala
@@ -1,8 +1,8 @@
 package fpinscala
 
-import org.scalatest.FunSuite
 import fpinscala.monoids.Monoid._
 import fpinscala.testing._
+import org.scalatest.FunSuite
 
 class ch10_MonoidsTests extends FunSuite {
 
@@ -15,10 +15,13 @@ class ch10_MonoidsTests extends FunSuite {
     Prop.run(monoidLaws(intMultiplication, Gen.smallInt))
     Prop.run(monoidLaws(booleanAnd, Gen.boolean))
     Prop.run(monoidLaws(booleanOr, Gen.boolean))
+
     def optionGen[A](g: Gen[A]): Gen[Option[A]] = g.flatMap(a => Gen.boolean.map(b => if (b) Some(a) else None))
     Prop.run(monoidLaws(optionMonoid[Int], optionGen(Gen.smallInt)))
+
     // TODO check below property: fails
-        def endoGen(g: Gen[Int]): Gen[Int => Int] = g.flatMap { a: Int => Gen.boolean.map(b => if (b) { x: Int => a * x } else { x: Int => a + x }) }
-        Prop.run(monoidLaws(endoMonoid[Int], endoGen(Gen.smallInt)))
+    def endoGen(g: Gen[Int]): Gen[Int => Int] = g.map { a: Int => x: Int => a * x }
+    //    def endoGen(g: Gen[Int]): Gen[Int => Int] = g.flatMap { a: Int => Gen.boolean.map(b => if (b) { x: Int => a * x } else { x: Int => a + x }) }
+    Prop.run(monoidLaws(endoMonoid[Int], endoGen(Gen.smallInt)))
   }
 }
