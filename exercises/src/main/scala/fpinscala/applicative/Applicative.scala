16738dc1486482417b1ea5da19127da863b0bb62
Hans L'hoëst <github@lhoest.eu>
2016-09-19 21:38:13 +0200

    Ch. 12 Applicative: exercises 12.1 till 12.3.

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -12,20 +12,40 @@ import language.implicitConversions
 
 trait Applicative[F[_]] extends Functor[F] {
 
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = {
+    val curr: A => B => C = f.curried
+    val f1: F[B => C] = map(fa)(curr)
+    apply(f1)(fb)
+  }
 
-  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = ???
+  def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B,C) => D): F[D] = {
+    val curr: A => B => C => D = f.curried
+    val f1: F[(A) => (B) => (C) => D] = unit(curr)
+    apply(apply(apply(f1)(fa))(fb))(fc)
+  }
+
+  def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B,C, D) => E): F[E] = {
+    val curr: A => B => C => D => E = f.curried
+    val f1: F[(A) => (B) => (C) => D => E] = unit(curr)
+    apply(apply(apply(apply(f1)(fa))(fb))(fc))(fd)
+  }
+
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fab, fa)(_(_))
 
   def unit[A](a: => A): F[A]
 
   def map[A, B](fa: F[A])(f: A => B): F[B] =
     apply(unit(f))(fa)
 
-  def sequence[A](fas: List[F[A]]): F[List[A]] = ???
+  def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(identity)
 
-  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] = ???
+  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] = {
+    as.foldRight(unit(List.empty[B])) { (a, acc) => map2(f(a), acc)(_ :: _) }
+  }
 
-  def replicateM[A](n: Int, fa: F[A]): F[List[A]] = ???
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((_, _))
+
+  def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
 
   def factor[A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
 
